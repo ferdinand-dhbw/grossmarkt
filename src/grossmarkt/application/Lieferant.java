da47c9c475755bcc9a5dbb222c4213e24443c0b0
ferdinand-dhbw <65330418+ferdinand-dhbw@users.noreply.github.com>
2021-04-15 11:59:30 +0200

    Implement Kunde, Produzent, Lieferant

--- src/grossmarkt/application/Lieferant.java
+++ src/grossmarkt/application/Lieferant.java
@@ -1,5 +1,8 @@
 package grossmarkt.application;
 
-public class Lieferant {
-
+public class Lieferant extends Person{
+  public Lieferant(String vorname, String nachname, String land, String stadt,
+      String strasse, String hausNr, int plz) {
+    super(vorname, nachname, land, stadt, strasse, hausNr, plz);
+  }
 }
--- src/grossmarkt/application/Produzent.java
+++ src/grossmarkt/application/Produzent.java
@@ -1,5 +1,8 @@
 package grossmarkt.application;
 
-public class Produzent {
-
+public class Produzent extends Person{
+  public Produzent(String vorname, String nachname, String land, String stadt,
+      String strasse, String hausNr, int plz) {
+    super(vorname, nachname, land, stadt, strasse, hausNr, plz);
+  }
 }
