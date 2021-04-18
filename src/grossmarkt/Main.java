abecadca2af540b75545d7a4fdece351dbfafed6
Jonas Rheiner <39375864+jrheiner@users.noreply.github.com>
2021-04-18 12:53:53 +0200

    Add alert and fix weather widget

--- src/grossmarkt/Main.java
+++ src/grossmarkt/Main.java
@@ -6,6 +6,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -13,7 +15,7 @@ public class Main extends Application {
   private static final MapReference MAP_REFERENCE = new MapReference();
 
   @Override
-  public void start(Stage primaryStage) throws Exception{
+  public void start(Stage homeStage) throws Exception{
     //Parent root = FXMLLoader.load(getClass().getResource("Home.fxml"));
 
     FXMLLoader loader = new FXMLLoader(getClass().getResource("Home.fxml"));
@@ -23,9 +25,11 @@ public class Main extends Application {
     homeController.setListReference(MAP_REFERENCE);
     homeController.initializeUI();
 
-    primaryStage.setTitle("Hello World");
-    primaryStage.setScene(new Scene(root, 1024, 576));
-    primaryStage.show();
+    homeStage.setTitle("Obst & Gemüse Meier oHG Verwaltung");
+    homeStage.setMinHeight(576);
+    homeStage.setMinWidth(1024);
+    homeStage.setScene(new Scene(root, homeStage.getMinWidth(), homeStage.getMinHeight()));
+    homeStage.show();
   }
 
 
--- src/grossmarkt/controller/HomeController.java
+++ src/grossmarkt/controller/HomeController.java
@@ -6,7 +6,12 @@ import java.time.format.DateTimeFormatter;
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
 import javafx.scene.text.Text;
 import javafx.util.Duration;
 
@@ -16,10 +21,22 @@ public class HomeController {
 
   @FXML
   private Text timestamp;
+  @FXML
+  private Button nav_kunde;
+  @FXML
+  private Button home_aufträge;
 
   public void initializeUI() {
     initClock();
+    initEvents();
   }
+
+  private void initEvents() {
+    EventHandler<ActionEvent> featureAlert = event -> featureAlert();
+    nav_kunde.setOnAction(featureAlert);
+    home_aufträge.setOnAction(featureAlert);
+  }
+
 
   private void initClock() {
     Timeline clock = new Timeline(new KeyFrame(Duration.ZERO, e -> {
@@ -32,6 +49,15 @@ public class HomeController {
 
   public void setListReference(MapReference mapReference) {
     this.mapReference = mapReference;
+  }
+
+  public void featureAlert() {
+    var alert = new Alert(AlertType.INFORMATION);
+    alert.setTitle("Funktion nicht verfügbar");
+    alert.setContentText("Die ausgewählte Funktion ist in dieser Version des Programms nocht nicht verfügbar.");
+    alert.setHeaderText(null);
+
+    alert.showAndWait();
   }
 
 }
