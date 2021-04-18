9e4df824289c6240ae040ad58fc06ac11120b8c8
Jonas Rheiner <39375864+jrheiner@users.noreply.github.com>
2021-04-18 12:18:30 +0200

    Add home view clock

--- src/grossmarkt/Main.java
+++ src/grossmarkt/Main.java
@@ -1,5 +1,6 @@
 package grossmarkt;
 
+import grossmarkt.controller.HomeController;
 import grossmarkt.maps.MapReference;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -13,14 +14,15 @@ public class Main extends Application {
 
   @Override
   public void start(Stage primaryStage) throws Exception{
-    Parent root = FXMLLoader.load(getClass().getResource("Home.fxml"));
-    /*
+    //Parent root = FXMLLoader.load(getClass().getResource("Home.fxml"));
+
     FXMLLoader loader = new FXMLLoader(getClass().getResource("Home.fxml"));
     Parent root = loader.load();
 
     HomeController homeController = loader.getController();
     homeController.setListReference(MAP_REFERENCE);
-     */
+    homeController.initializeUI();
+
     primaryStage.setTitle("Hello World");
     primaryStage.setScene(new Scene(root, 1024, 576));
     primaryStage.show();
--- src/grossmarkt/controller/HomeController.java
+++ src/grossmarkt/controller/HomeController.java
@@ -1,5 +1,37 @@
 package grossmarkt.controller;
 
+import grossmarkt.maps.MapReference;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+import javafx.util.Duration;
+
 public class HomeController {
+
+  private MapReference mapReference;
+
+  @FXML
+  private Text timestamp;
+
+  public void initializeUI() {
+    initClock();
+  }
+
+  private void initClock() {
+    Timeline clock = new Timeline(new KeyFrame(Duration.ZERO, e -> {
+      DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy\nHH:mm:ss");
+      timestamp.setText(LocalDateTime.now().format(formatter));
+    }), new KeyFrame(Duration.seconds(1)));
+    clock.setCycleCount(Animation.INDEFINITE);
+    clock.play();
+  }
+
+  public void setListReference(MapReference mapReference) {
+    this.mapReference = mapReference;
+  }
 
 }
