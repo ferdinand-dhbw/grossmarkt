517c27b6407aee003606d5f512952ee6dc832efe
ferdinand-dhbw <65330418+ferdinand-dhbw@users.noreply.github.com>
2021-04-19 18:14:12 +0200

    First Implementation of LieferantMap.java

--- src/grossmarkt/maps/LieferantMap.java
+++ src/grossmarkt/maps/LieferantMap.java
@@ -1,5 +1,24 @@
 package grossmarkt.maps;
 
-public class LieferantMap {
+import grossmarkt.application.Lieferant;
+import java.util.Collections;
+import java.util.HashMap;
 
+public class LieferantMap {
+  private HashMap<Integer, Lieferant> lieferantHashMap;
+
+  public HashMap<Integer, Lieferant> getLieferantHashMap() {
+    return lieferantHashMap;
+  }
+
+  public void addLieferant(Lieferant lieferant){
+    int nextFreeKey = lieferantHashMap.isEmpty() ? 0 : Collections.max(lieferantHashMap.keySet()) +1;
+    lieferantHashMap.put(nextFreeKey, lieferant);
+  }
+
+  public void populateWithDemodata(){
+    addLieferant(new Lieferant("Ann", "Geber", "DE", "Stuttgart", "Schulstrasse", "3a", 70174));
+    addLieferant(new Lieferant("Jo", "Ghurt", "GB", "London", "Mainstreet", "420", 424242));
+    addLieferant(new Lieferant("Knut", "Schfleck", "DE", "Muenchen", "Wilde Maus", "0", 80331));
+  }
 }
